b97719df59c838ca1df646adad46728f76ace537
Igor Kirpinev <kirpinevigor@yandex.ru>
2024-10-11 17:37:39 +0300

    fix

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -32,7 +32,7 @@ export const ThxLayout = () => {
         <ButtonMobile
           block
           view="primary"
-          href="alfabank:///multistep-route?fromModule=FORM&stepNumber=0&alias=alfa-subscription-alias&version=2&source=ghyouth"
+          href="alfabank://dashboard/"
         >
           Хорошо
         </ButtonMobile>
